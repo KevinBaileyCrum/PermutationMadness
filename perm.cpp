52cea0cf0d94f885ca1ea6eaeccefc4c182c85b3
Kevin Crum <kevinbaileycrum@gmail.com>
2018-03-27 00:16:45 -0700

    the ol' going to bed commit.  Goodnight everyone

--- perm.cpp
+++ perm.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char** argv){
+    // cmndln args error handling
     if( argc<2 ){
         cout<<"usage: ./perm filename"<<endl;
         exit( EXIT_FAILURE );
     }
     
-    string line;
+    // Get OG string: OGList
+    // store in temp string: NewList
+    string OGList;
     string opps;
     ifstream inFile( argv[1] );
-        getline( inFile, line );
-        cout<<line<<endl;
-        cout<<line[1]<<endl;
+        getline( inFile, OGList );
+        cout<<OGList<<endl;
+        
+        vector<char> NewList;
+        for( int i=0; i<OGList.length(); ++i ){
+            NewList.push_back( OGList[i] );
+        }
 
         getline( inFile, opps );
         cout<<opps<<endl;
